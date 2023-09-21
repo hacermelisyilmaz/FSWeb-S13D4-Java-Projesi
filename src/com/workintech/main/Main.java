33840df1dc43dae1741500e0a8d115f85090af41
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-21 15:16:30 +0300

    Point class test are carried out

--- src/com/workintech/main/Main.java
+++ src/com/workintech/main/Main.java
@@ -1,7 +1,17 @@
 package com.workintech.main;
 
+import com.workintech.model.Point;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Point Sınıfı:");
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point(0,0);
+        System.out.println("distance()= " + point.distance());
+        System.out.println("******************");
     }
 }
