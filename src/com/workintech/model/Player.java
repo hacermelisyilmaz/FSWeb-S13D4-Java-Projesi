64a0c07e1ad35f4dbc9a368748796c0b4a16c89c
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-21 16:10:56 +0300

    Methods are created in Player class

--- src/com/workintech/model/Player.java
+++ src/com/workintech/model/Player.java
@@ -10,4 +10,20 @@ public class Player {
         if (healthPercentage > 100) this.healthPercentage = 100;
         else this.healthPercentage = Math.max(healthPercentage, 0);
     }
+
+    public int healthRemaining() {
+        return healthPercentage;
+    }
+
+    public void loseHealth(int damage) {
+        healthPercentage -= damage;
+        if (healthPercentage <= 0) {
+            System.out.println(name + " player has been knocked out of game");
+            healthPercentage = 0;
+        }
+    }
+
+    public void restoreHealth(int healthPotion) {
+        healthPercentage = Math.min(healthPercentage + healthPotion, 100);
+    }
 }
